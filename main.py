96837289d86a23a91017e8791ee60bbc69606f6f
hyperbestia <d.guti@estudiante.uc.cl>
2026-03-26 10:52:27 -0300

    implementacion de logica de ciclo de juego

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ opciones = ['PIEDRA', 'PAPEL', 'TIJERA', 'LAGARTO', 'SPOCK']
 
 def jugar(eleccion):
     pass
+
+while True:
+    try:
+        opcion_usuario = input('Ingresa tu movimiento muajajaja: ')
+        if opcion_usuario not in opciones:
+            raise ValueError
+        else:
+            break
+    except ValueError:
+        print('Elige bien :v, coloca una opción de las que te coloqué')
+jugar()
+    
